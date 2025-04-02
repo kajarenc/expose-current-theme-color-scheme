978dc2b62a39e4c0410c5e2d30d8a049fca480ee
Karen Javadyan <kajarenc@gmail.com>
2025-04-02 19:24:31 +0400

    add image

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ st.write(
     f"Your current theme color scheme is: **:rainbow[{st.context.theme_color_scheme}]**"
 )
 
+st.write("And here is dog image:")
+
+if st.context.theme_color_scheme == "light":
+    st.image("doglight.jpg")
+else:
+    st.image("dogdark.jpg")
+
 
 x = st.slider("Enter a number", 0, 20, 0)
 st.write(f"Your current slider value is: {x}")
